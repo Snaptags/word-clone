332c44ce80c07de910a52a04c3b8741d9ed86cd1
Markus Lasermann <github@ml74.de>
2023-06-02 16:57:11 +0200

    feat: Exercise 1: GuessInput

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const GuessInput = ({ onGuess }) => {
+  const [guess, setGuess] = React.useState("");
+  const onChange = (event) => {
+    setGuess(event.target.value.toUpperCase().replace(/[^a-z]/gi, ""));
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    onGuess?.(guess);
+    console.info({ guess });
+    setGuess("");
+  };
+  return (
+    <form class="guess-input-wrapper" onSubmit={onSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        autoComplete="off"
+        type="text"
+        pattern="[A-Za-z]{5}"
+        title="Must contain exactly 5 letters a-z"
+        maxLength={5}
+        onChange={onChange}
+        value={guess}
+      />
+    </form>
+  );
+};
+
+export default GuessInput;
